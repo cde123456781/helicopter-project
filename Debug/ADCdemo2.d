8177aab8c4f3660dccef008fbddbaaaddbc8ef2a
Bryson Chen <bjc199@uclive.ac.nz>
2024-03-14 12:20:52 +1300

    Initial commit

---
+++ Debug/ADCdemo2.d
@@ -0,0 +1,87 @@
+# FIXED
+
+ADCdemo2.obj: ../ADCdemo2.c
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/adc.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pwm.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/systick.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h
+ADCdemo2.obj: C:/ti/TivaWare_C_Series-2.2.0.295/utils/ustdlib.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/time.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/abi_prefix.h
+ADCdemo2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_time.h
+ADCdemo2.obj: ../circBufT.h
+ADCdemo2.obj: P:/Courses/ENCE361/labs/OrbitOLED/OrbitOLEDInterface.h
+
+../ADCdemo2.c:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/adc.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/pwm.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/systick.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/debug.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/utils/ustdlib.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/time.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/abi_prefix.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_time.h:
+
+../circBufT.h:
+
+P:/Courses/ENCE361/labs/OrbitOLED/OrbitOLEDInterface.h:
+
---
+++ Debug/OrbitOLED/lib_OrbitOled/FillPat.d
@@ -0,0 +1,6 @@
+# FIXED
+
+OrbitOLED/lib_OrbitOled/FillPat.obj: P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/FillPat.c
+
+P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/FillPat.c:
+
