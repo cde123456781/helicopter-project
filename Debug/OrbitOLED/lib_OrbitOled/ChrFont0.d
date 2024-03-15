d9a26812d15b85c939ca67484333babb486b5ee8
Bryson Chen <bjc199@uclive.ac.nz>
2024-03-15 14:26:43 +1300

    Update .gitignore

--- Debug/OrbitOLED/lib_OrbitOled/ChrFont0.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-OrbitOLED/lib_OrbitOled/ChrFont0.obj: P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/ChrFont0.c
-
-P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/ChrFont0.c:
-
--- Debug/OrbitOLED/lib_OrbitOled/FillPat.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-OrbitOLED/lib_OrbitOled/FillPat.obj: P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/FillPat.c
-
-P:/Courses/ENCE361/Labs/OrbitOLED/lib_OrbitOled/FillPat.c:
-
